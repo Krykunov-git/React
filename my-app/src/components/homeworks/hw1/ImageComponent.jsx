32c9c128a7aa85dc9100c90fffd4cf4b568ed6ff
Ivan <honda27nn@gmail.com>
2025-07-06 15:31:05 +0200

    fix alt text

--- my-app/src/components/homeworks/hw1/ImageComponent.jsx
+++ my-app/src/components/homeworks/hw1/ImageComponent.jsx
@@ -1,6 +1,6 @@
 function Image () {
 
-    return( <img   src="https://storage.yandexcloud.net/storage.yasno.media/nat-geo/images/2020/6/25/0fc978aba29e466e8eb4ffc946532d5e.max-2000x1000.jpg"></img>)
+    return( <img   src="https://storage.yandexcloud.net/storage.yasno.media/nat-geo/images/2020/6/25/0fc978aba29e466e8eb4ffc946532d5e.max-2000x1000.jpg"alt=""></img>)
 }
 
 export default Image 
