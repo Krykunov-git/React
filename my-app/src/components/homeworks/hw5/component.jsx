dac2289d87f146b43ad66b3b668df0838a020a92
Ivan <honda27nn@gmail.com>
2025-07-16 23:17:32 +0200

    add logo

--- my-app/src/components/homeworks/hw5/component.jsx
+++ my-app/src/components/homeworks/hw5/component.jsx
@@ -1,11 +1,16 @@
 import Logo from "../../homeworks/hw5/assets/logo.svg"
-
+import styles from './component.module.css';
 
 function Component(){
 
     return(
-        <div>
-            <img src={Logo} alt="logo" />
+        <div className={styles.section}>
+            <div className={styles.logo}>
+                <img src={Logo} alt="logo" />
+                <h1 className={styles.logo_title}>Spotify</h1>
+
+            </div>
+            
             <h1></h1>
         </div>
     )
