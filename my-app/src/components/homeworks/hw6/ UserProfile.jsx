8039d179404cfaeb95960c2486da0517deb5cae8
Ivan <honda27nn@gmail.com>
2025-07-28 00:17:01 +0200

    finish

--- my-app/src/components/homeworks/hw6/ UserProfile.jsx
+++ my-app/src/components/homeworks/hw6/ UserProfile.jsx
@@ -3,13 +3,9 @@ import { useState,useEffect } from "react"
 import styles from "./UserProfile.module.css"
 
 
-
-
-
-
 function User(){
     const[users,setUsers]=useState(null)
-    async function fetchUser() {
+     function fetchUser() {
     axios 
     .get("https://randomuser.me/api")
     .then((respons)=>setUsers(respons.data.results[0]))
@@ -22,11 +18,13 @@ function User(){
 
 
     return(
-        <div>{users === null ?<p>Loading...</p>: <div>  <img src={users.picture.large}  alt="User" /> 
-        <p>{users.name.first} {users.name.last}</p> 
-        <p>{users.email}</p>
-         <p>{users.phone}</p>
-        <button onClick={()=> fetchUser()} >Load New User</button></div>}
+        <div className={styles.card}>{users === null ?<p>Loading...</p>: 
+        <div>  <img className={styles.img} src={users.picture.large}  alt="User" /> 
+         <p className={styles.name}>{users.name.first} {users.name.last}</p>
+       <p className={styles.email}>{users.email}</p>
+  <p className={styles.phone}>{users.phone}</p>
+  <button onClick={fetchUser} className={styles.button}>Load New User</button>
+        </div>}
        
        
         </div>
